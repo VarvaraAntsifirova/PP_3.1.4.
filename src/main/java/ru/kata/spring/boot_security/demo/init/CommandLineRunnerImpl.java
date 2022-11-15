411820999d3ed11245f6e6b53070847fefcf7870
Varvara Antsifirova <antivarya104@gmail.com>
2022-11-15 09:50:27 +0300

    The last commit

---
+++ src/main/java/ru/kata/spring/boot_security/demo/init/CommandLineRunnerImpl.java
@@ -0,0 +1,40 @@
+package ru.kata.spring.boot_security.demo.init;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import ru.kata.spring.boot_security.demo.model.User;
+import ru.kata.spring.boot_security.demo.repository.UserRepository;
+
+@Component
+public class CommandLineRunnerImpl implements CommandLineRunner {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public CommandLineRunnerImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        User admin = new User();
+        admin.setUsername("Admin");
+        admin.setPassword("$2y$10$aHyB0C.gDd4p2U.jLBCS8ep6U4eh/CFSvZUCM8OTAcuAUb2h5Crgq"); //100
+        admin.setFirstName("Admin");
+        admin.setLastName("Admin");
+        admin.setAge(18);
+        admin.setRoles(new String[]{"ROLE_ADMIN"});
+        userRepository.save(admin);
+
+        User user = new User();
+        user.setUsername("User");
+        user.setPassword("$2y$10$cTd02aDowqkoLP/x8W.tuubXUsKlEcc06/EoDafv6g66SEwxxbYw2"); //200
+        user.setFirstName("User");
+        user.setLastName("User");
+        user.setAge(24);
+        user.setRoles(new String[]{"ROLE_USER"});
+        userRepository.save(user);
+
+    }
+}
