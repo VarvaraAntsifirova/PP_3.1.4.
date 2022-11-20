7fa452eeafc0d44aa0cbed367f073bdd11159220
Varvara Antsifirova <antivarya104@gmail.com>
2022-11-20 15:40:37 +0300

    last commit

--- src/main/java/ru/kata/spring/boot_security/demo/service/UserServiceImpl.java
+++ src/main/java/ru/kata/spring/boot_security/demo/service/UserServiceImpl.java
@@ -11,8 +11,11 @@ import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import ru.kata.spring.boot_security.demo.model.Role;
 import ru.kata.spring.boot_security.demo.model.User;
+import ru.kata.spring.boot_security.demo.repository.RoleRepository;
 import ru.kata.spring.boot_security.demo.repository.UserRepository;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -23,11 +26,14 @@ public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
 
+    private final RoleRepository roleRepository;
+
     private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public UserServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    public UserServiceImpl(UserRepository userRepository, RoleRepository roleRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
         this.passwordEncoder = passwordEncoder;
     }
 
@@ -90,5 +96,36 @@ public class UserServiceImpl implements UserService {
     @Override
     public User findByUsername(String username) {
         return userRepository.findUserByUsername(username);
+    }
+
+    @PostConstruct
+    public void initialCreatingOfEntities() {
+        Role roleAdmin = new Role();
+        roleAdmin.setRoleName("ROLE_ADMIN");
+        roleAdmin.setId(1);
+        roleRepository.save(roleAdmin);
+
+        Role roleUser = new Role();
+        roleUser.setRoleName("ROLE_USER");
+        roleUser.setId(2);
+        roleRepository.save(roleUser);
+
+        User admin = new User();
+        admin.setUsername("admin@mail.ru");
+        admin.setPassword("$2y$10$aHyB0C.gDd4p2U.jLBCS8ep6U4eh/CFSvZUCM8OTAcuAUb2h5Crgq"); //100
+        admin.setFirstName("Admin");
+        admin.setLastName("Admin");
+        admin.setAge(18);
+        admin.setRoles(new String[]{"ROLE_ADMIN", "ROLE_USER"});
+        userRepository.save(admin);
+
+        User user = new User();
+        user.setUsername("user@mail.ru");
+        user.setPassword("$2y$10$cTd02aDowqkoLP/x8W.tuubXUsKlEcc06/EoDafv6g66SEwxxbYw2"); //200
+        user.setFirstName("User");
+        user.setLastName("User");
+        user.setAge(24);
+        user.setRoles(new String[]{"ROLE_USER"});
+        userRepository.save(user);
     }
 }
